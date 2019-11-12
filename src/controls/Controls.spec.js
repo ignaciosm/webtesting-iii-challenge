90bbafc2e7fa156870c5f82d252d2f8b053e18fe
ignaciosm <ignaciosm@gmail.com>
2019-11-11 19:47:29 -0500

    mvp

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,56 @@
-// Test away!
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react';
+import Controls from './Controls';
+
+test('Controls is present', () => {
+  render(<Controls/>);
+});
+
+test('disable lock if gate open', () => {
+  const lockedMock = jest.fn();
+  const { queryByText } = render(<Controls toggleLocked={lockedMock} />);
+  const lockButton = queryByText(/lock gate/i);
+  expect(lockButton.disabled).toEqual(true);
+});
+
+test('disable open if gate locked', () => {
+  const { queryByText } = render(<Controls locked={true} closed={true} />);
+  const openButton = queryByText(/open gate/i);
+  expect(openButton.disabled).toEqual(true);
+});
+
+test('close is null if gate locked', () => {
+  const { queryByText } = render(<Controls locked={true} closed={true} />);
+  const closeButton = queryByText(/close gate/i);
+  expect(closeButton).toBeNull();
+});
+
+test('toggle close gets called', () => {
+  const closeMock = jest.fn();
+  const { queryByText } = render(<Controls locked={false} closed={false} toggleClosed={closeMock} />);
+  const closeButton = queryByText(/close gate/i);
+
+  fireEvent.click(closeButton);
+  expect(closeMock).toHaveBeenCalled();
+});
+
+test('toggle lock gets called', () => {
+  const lockeMock = jest.fn();
+  const { queryByText } = render(<Controls locked={false} closed={true} toggleLocked={lockeMock} />);
+  const closeButton = queryByText(/lock gate/i);
+
+  fireEvent.click(closeButton);
+  expect(lockeMock).toHaveBeenCalled();
+});
+
+test('button text shows close if door open', () => {
+  const { queryByText } = render(<Controls locked={false} closed={false} />);
+  const closeButton = queryByText(/close gate/i);
+  expect(closeButton).toBeTruthy();
+});
+
+test('button text shows open if door closed', () => {
+  const { queryByText } = render(<Controls locked={false} closed={true} />);
+  const closeButton = queryByText(/open gate/i);
+  expect(closeButton).toBeTruthy();
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,17 @@
-// Test away
+import React from 'react';
+import {render} from '@testing-library/react';
+import Dashboard from './Dashboard';
+
+test('Dashboard is present', () => {
+  render(<Dashboard/>);
+});
+
+test('Initial state is Unlocked', () => {
+  const {queryByText} = render(<Dashboard/>);
+  expect(queryByText(/unlocked/i)).toBeTruthy()
+});
+
+test('Initial state is Open', () => {
+  const {queryByText} = render(<Dashboard/>);
+  expect(queryByText(/open/i)).toBeTruthy()
+});
